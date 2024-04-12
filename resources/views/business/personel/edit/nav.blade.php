631a8b7f93f0f95a583922b701919bbd2671f93f
migrain.code <migrain.code@gmail.com>
2024-04-12 06:15:42 +0300

    -- müşteri detay görsel düzneleme

--- resources/views/business/personel/edit/nav.blade.php
+++ resources/views/business/personel/edit/nav.blade.php
@@ -5,7 +5,7 @@
         <div class="d-flex flex-wrap flex-sm-nowrap mb-6">
             <!--begin::Image-->
             <div class="d-flex flex-center flex-shrink-0 bg-light rounded w-100px h-100px w-lg-150px h-lg-150px me-7 mb-4">
-                <img class="mw-50px mw-lg-75px" src="{{image($personel->image)}}" alt="image">
+                <img class="w-100 rounded" src="{{image($personel->image)}}" alt="image">
             </div>
             <!--end::Image-->
 
