ec130aa3b27743824acb7882044dcf7363c1513d
migrain.code <migrain.code@gmail.com>
2024-04-12 06:20:21 +0300

    -- müşteri detay görsel düzneleme

--- resources/views/business/dashboard/parts/side-personel.blade.php
+++ resources/views/business/dashboard/parts/side-personel.blade.php
@@ -16,11 +16,12 @@
         <!--begin::Navbar-->
         <div class="app-navbar flex-column flex-center py-4" style="position: fixed;padding: 10px;">
             <!--begin::Navbar item-->
-            <a href="pages/user-profile/overview.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-2.jpg" class="" alt="">
-                </div>
-                <div class="bg-dark customName" style="width: 230px;
+            @foreach(authUser()->business->personels as $personel)
+                <a href="{{route('business.personel.edit', $personel->id)}}" class="btn btn-icon btn-default mx-auto mb-4 ">
+                    <div class="symbol symbol-40px">
+                        <img src="{{image($personel->image)}}" class="" alt="">
+                    </div>
+                    <div class="bg-dark customName" style="width: 230px;
                                     position: absolute;
                                     left: 52px;
                                     background-color: #1e1e2d !important;
@@ -28,75 +29,11 @@
                                     border-top-right-radius: 15px;
                                     border-bottom-right-radius: 15px;
                                     border: 1px solid #1e1e2D;">
-                    Muhammet Türkmen
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="account/billing.html" class="btn btn-icon btn-default mx-auto mb-4 active">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-7.jpg" class="" alt="">
-                </div>
-                <div class="bg-dark customName" style="width: 230px;
-                                    position: absolute;
-                                    left: 52px;
-                                    background-color: #1e1e2d !important;
-                                    padding: 15px;
-                                    border-top-right-radius: 15px;
-                                    border-bottom-right-radius: 15px;
-                                    border: 1px solid #1e1e2D;">
-                    Muhammet Türkmen
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="account/activity.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-10.jpg" class="" alt="">
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="account/overview.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-1.jpg" class="" alt="">
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="account/settings.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-3.jpg" class="" alt="">
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="apps/projects/users.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-21.jpg" class="" alt="">
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="apps/projects/targets.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-6.jpg" class="" alt="">
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="apps/projects/files.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-13.jpg" class="" alt="">
-                </div>
-            </a>
-            <!--end::Navbar item-->
-            <!--begin::Navbar item-->
-            <a href="apps/user-management/users/list.html" class="btn btn-icon btn-default mx-auto mb-4 ">
-                <div class="symbol symbol-40px">
-                    <img src="/business/assets/media/avatars/300-20.jpg" class="" alt="">
-                </div>
-            </a>
+                        {{$personel->name}}
+                    </div>
+                </a>
+
+            @endforeach
             <!--end::Navbar item-->
 
             <!--begin::Separator-->
