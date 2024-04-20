143f6d1138f1309628625a3b1ab1de5c43ab426b
migrain.code <migrain.code@gmail.com>
2024-04-20 12:47:09 +0300

    -- blog görseli test

--- resources/views/blogs/parts/slider.blade.php
+++ resources/views/blogs/parts/slider.blade.php
@@ -3,25 +3,29 @@
         <div class="row">
             <div class="col-12">
                 <div class="bigSliderContent">
-
                     <div class="owl-carousel owl-theme">
-                        @foreach($blogAdvrt as $advert)
-                            <!--Slider ıtem start-->
-                            <div class="item">
-                                <a href="{{$advert->link}}">
-                                    <img src="{{image($advert->image)}}" alt="" />
-                                </a>
-                            </div>
-                            <!--Slider ıtem end-->
-                        @endforeach
-
+                        <div class="item">
+                            <a href="javascript:;">
+                                <img src="assets/images/bigSlider.png" alt="" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="javascript:;">
+                                <img src="assets/images/bigSlider.png" alt="" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="javascript:;">
+                                <img src="assets/images/bigSlider.png" alt="" />
+                            </a>
+                        </div>
                     </div>
                     <div class="sliderArrow">
                         <a href="javascript:;" class="sliderPrev">
-                            <img src="/front/assets/images/icons/ico-slider-left.svg" />
+                            <img src="assets/images/icons/ico-slider-left.svg" />
                         </a>
                         <a href="javascript:;" class="sliderNext">
-                            <img src="/front/assets/images/icons/ico-slider-right.svg" />
+                            <img src="assets/images/icons/ico-slider-right.svg" />
                         </a>
                     </div>
                 </div>
