1408b5f395434e9d29e021bd4f4e9fba2760e636
migrain.code <migrain.code@gmail.com>
2024-04-21 07:01:43 +0300

    -- login reklam görselleri eklendi

--- resources/views/personel/auth/login.blade.php
+++ resources/views/personel/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('title', 'Yetkili Girişi')
+@section('title', 'Personel Girişi')
 @section('content')
     <div class="formBox">
         <div class="formBoxContent d-md-flex align-items-center">
