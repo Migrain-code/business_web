ecfcf0f518435b910a384b46ad4b143ae4d6484f
migrain.code <migrain.code@gmail.com>
2024-04-23 14:44:55 +0300

    -- menü düzeni yeniden ayarlandı

--- resources/views/personel/layouts/menu/top.blade.php
+++ resources/views/personel/layouts/menu/top.blade.php
@@ -122,10 +122,10 @@
                          id="kt_menu_notifications">
                         <!--begin::Heading-->
                         <div class="d-flex flex-column bgi-no-repeat rounded-top"
-                             style="background-image:url('assets/media/misc/menu-header-bg.jpg')">
+                             style="background-image:url('/business/assets/media/misc/menu-header-bg.jpg')">
                             <!--begin::Title-->
                             <h3 class="text-white fw-semibold px-9 mt-10 mb-6">
-                                Notifications <span class="fs-8 opacity-75 ps-3">24 reports</span>
+                                Bildirimler <span class="fs-8 opacity-75 ps-3">{{authUser()->notificationMenu->count()}} Bildirim</span>
                             </h3>
                             <!--end::Title-->
 
