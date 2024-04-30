8c507899af6dcb59a9d8516b2b8094a47618753b
migrain.code <migrain.code@gmail.com>
2024-04-30 17:29:12 +0300

    özellikler sayfasında alttaki istatistik alanlarını aç

--- resources/views/main-page/parts/stats.blade.php
+++ resources/views/main-page/parts/stats.blade.php
@@ -9,16 +9,14 @@
 
         <div class="row">
             <div class="col-lg-12">
-                <div
-                    class="video-area"
-                    data-bs-toggle="modal"
-                    data-bs-target="#videoModal"
-                >
+                @if(strpos(setting('business_main_page_video_link'), "https://") === 0)
+                <div class="video-area" data-bs-toggle="modal" data-bs-target="#videoModal">
                     <img src="{{image(setting('business_main_page_video_image'))}}" alt=""/>
                     <a href="javascript:;" class="play-video">
                         <img src="/front/assets/images/business/video-button.svg" alt=""/>
                     </a>
                 </div>
+                @endif
                 @if($stats->count() > 0)
                     <div class="row stats-items">
                         @foreach($stats as $stat)
