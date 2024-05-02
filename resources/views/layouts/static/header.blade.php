6b6c7b7ffbf16fd1f4a81e8605e66744719250fc
migrain.code <migrain.code@gmail.com>
2024-05-02 17:51:59 +0300

    -- prices güncellendi

--- resources/views/layouts/static/header.blade.php
+++ resources/views/layouts/static/header.blade.php
@@ -52,7 +52,7 @@
         }
         @media (max-width: 500px) {
             .home-stats {
-                padding: 20px 20px;
+                padding: 20px 20px !important;
             }
         }
     </style>
