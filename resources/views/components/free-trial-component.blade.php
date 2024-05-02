8d6bc2f13a21f5c0743330b141d46f38086d3fa6
migrain.code <migrain.code@gmail.com>
2024-05-02 16:48:05 +0300

    -- free-trial-component.blade.php güncellendi

--- resources/views/components/free-trial-component.blade.php
+++ resources/views/components/free-trial-component.blade.php
@@ -9,7 +9,7 @@
                     </h2>
                     <div class="buttons">
                         <a href="{{route('business.home')}}"> 30 Gün Ücretsiz Dene </a>
-                        <a href="{{route('business.home')}}">Ücretsiz Deneme</a>
+                        <a href="{{route('business.contact')}}">Bize Ulaşın</a>
                     </div>
                 </div>
                 <div class="man-image">
