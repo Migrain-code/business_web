0f9a2f670ce127a40e809d75fb2c2684166905e3
migrain.code <migrain.code@gmail.com>
2024-05-06 21:07:31 +0300

    -- tawk chat eklendi

--- resources/views/business/layouts/components/styles.blade.php
+++ resources/views/business/layouts/components/styles.blade.php
@@ -11,19 +11,7 @@
 <link href="/business/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 <link href="/business/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
 <link href="/business/assets/css/custom.css" rel="stylesheet" type="text/css" />
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/6639124b07f59932ab3ca2f7/1ht7eutps';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
+
 <style>
     ::-webkit-scrollbar{
         width: 5px;
