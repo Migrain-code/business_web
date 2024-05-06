bc9a85dc61554fdc65f1483347abce1331b9b899
migrain.code <migrain.code@gmail.com>
2024-05-06 18:47:14 +0300

    -- paket sayışı düzenlendi

--- resources/views/business/package-sale/edit/columns/col-2.blade.php
+++ resources/views/business/package-sale/edit/columns/col-2.blade.php
@@ -7,18 +7,18 @@
                 <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-semibold mb-8">
                     <!--begin:::Tab item-->
                     <li class="nav-item">
-                        <a class="nav-link text-active-primary pb-4 active payments" data-package="{{$packageSale->id}}" style="font-size: 2rem;" data-bs-toggle="tab"
+                        <a class="nav-link text-active-primary pb-4 active payments" data-package="{{$packageSale->id}}" style="font-size: 1.25rem;" data-bs-toggle="tab"
                            href="#kt_ecommerce_customer_payments">Ödemeler</a>
                     </li>
                     <!--end:::Tab item-->
                     <!--begin:::Tab item-->
                     <li class="nav-item">
-                        <a class="nav-link text-active-primary pb-4 usages" data-package="{{$packageSale->id}}" style="font-size: 2rem;" data-bs-toggle="tab"
+                        <a class="nav-link text-active-primary pb-4 usages" data-package="{{$packageSale->id}}" style="font-size: 1.25rem;" data-bs-toggle="tab"
                            href="#kt_ecommerce_customer_usages">Kullanımlar</a>
                     </li>
                     <!--end:::Tab item-->
                     <li class="nav-item">
-                        <a class="nav-link text-active-primary pb-4 usages" data-package="{{$packageSale->id}}" style="font-size: 2rem;" data-bs-toggle="tab"
+                        <a class="nav-link text-active-primary pb-4 usages" data-package="{{$packageSale->id}}" style="font-size: 1.25rem;" data-bs-toggle="tab"
                            href="#kt_ecommerce_customer_policy">Sözleşmeler</a>
                     </li>
                     <!--end:::Tab item-->
