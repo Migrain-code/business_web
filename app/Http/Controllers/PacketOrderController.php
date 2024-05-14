1152b549219515642d65364cf32b2e8840159432
migrain.code <migrain.code@gmail.com>
2024-05-14 17:38:48 +0300

    -- iyzico test işlemleri

--- app/Http/Controllers/PacketOrderController.php
+++ app/Http/Controllers/PacketOrderController.php
@@ -94,32 +94,6 @@ class PacketOrderController extends Controller
         echo $response->getHtmlContent();
     }
 
-    public function callback(Request $request, BussinessPackage $packet, BusinessOfficial $official)
-    {
-        Auth::guard('official')->loginUsingId($official->id);
-
-        $payment = (new Iyzico())->completePayment($request->paymentId);
-
-        if ($payment->getStatus() == 'success') {
-
-            $packetOrder = new PacketOrder();
-            $packetOrder->packet_id = $packet->id;
-            $packetOrder->company_id = $this->business->id;
-            $packetOrder->price = $payment->getPrice();
-            $packetOrder->tax = $request->kdv ?? 0;
-            $packetOrder->discount = $request->discount ?? 0;
-            $packetOrder->payment_id = $payment->getPaymentId();
-            $packetOrder->payment_type = 'CARD';
-            $packetOrder->save();
-
-            $business = Business::find(authUser()->business->id);
-
-            //$this->createInvoice($packetOrder->id, $packetOrder->price - $packetOrder->tax, $packet->id = 43841443, $company->getParachuteId());
-
-            return to_route('business.packet.payment.success', ['siparis-no' => $packetOrder->id]);
-        }
-        return to_route('business.packet.payment.fail');
-    }
 
     public function success()
     {
--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ use \App\Http\Controllers\DetailSetupController;
 use \App\Http\Controllers\AppointmentRequestFormController;
 use App\Http\Controllers\Business\Form\BusinessAppointmentRequestController;
 use App\Http\Controllers\PacketOrderController;
+use App\Http\Controllers\PaymentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -88,7 +89,7 @@ Route::prefix('isletme')->as('business.')->group(function (){
     Route::get('/sifremi-unuttum-dogrulama-tekrar-gönder', [ForgotPasswordController::class, 'verifyResetRepeatPassword'])->name('verify.repeatPassword');
 
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-    Route::post('/packet/{packet}/callback/{official}', [PacketOrderController::class, 'callback'])->name('packet.payment.callback');
+    Route::post('/packet/{packet}/callback/{official}', [PaymentController::class, 'callback'])->name('packet.payment.callback');
     Route::middleware(['auth:official', 'setup'])->group(function () {
 
         Route::get('/home', [\App\Http\Controllers\Business\HomeController::class, 'index'])->name('home');
