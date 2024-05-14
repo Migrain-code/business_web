33613c9075b1ce228bf28a2331f6a71fb2c3d7b1
migrain.code <migrain.code@gmail.com>
2024-05-14 17:36:27 +0300

    -- iyzico test işlemleri

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/isveren/packet/callback/*',
+        '/isletme/packet/*/callback/*',
     ];
 }
