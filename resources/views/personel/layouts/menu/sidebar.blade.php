b542d61e9c91a2b941d1eb19d1034fbf3edfcdb8
migrain.code <migrain.code@gmail.com>
2024-05-18 19:19:41 +0300

    -- personel randeuv ayarı yapıldı

--- resources/views/personel/layouts/menu/sidebar.blade.php
+++ resources/views/personel/layouts/menu/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!--begin::Sidebar-->
 <div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="225px" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
     <!--begin::Logo-->
-    <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
+    <div class="app-sidebar-logo ps-3" id="kt_app_sidebar_logo">
         <a href="{{route('personel.home')}}">
             <img alt="Logo" src="{{image(setting('business_logo_white'))}}" class="h-25px app-sidebar-logo-default" />
             <img alt="Logo" src="/business/assets/media/logos/minimize-logo.svg" class="h-30px app-sidebar-logo-minimize" />
