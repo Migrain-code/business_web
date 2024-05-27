aa2e3fafb32ec79467c0e112a4c971054e8f5c6d
migrain.code <migrain.code@gmail.com>
2024-05-27 14:36:02 +0300

    -- personel randevu oluşturma düznelendi

--- resources/views/personel/appointment/edit/index.blade.php
+++ resources/views/personel/appointment/edit/index.blade.php
@@ -180,8 +180,7 @@
                                             <div class="symbol symbol-circle symbol-25px overflow-hidden me-3">
                                                 <a href="#">
                                                     <div class="symbol-label">
-                                                        <img src="{{image($appointment->customer->image)}}"
-                                                             alt="{{$appointment->customer->name}}" class="w-100">
+
                                                     </div>
                                                 </a>
                                             </div>
