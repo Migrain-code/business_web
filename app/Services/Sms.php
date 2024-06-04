62ff25241b35a15f14b236459d2040ad1cee3bab
migrain.code <migrain.code@gmail.com>
2024-06-04 15:23:41 +0300

    -- hizmetlere onay türü eklendi

--- app/Services/Sms.php
+++ app/Services/Sms.php
@@ -9,7 +9,7 @@ class Sms
     private static string $apiUrl = "https://api.netgsm.com.tr/bulkhttppost.asp";
     const username = '4646060976';
     const password = 'V3.7CHR5';
-    const title = 'Hizlirandvu';
+    const title = 'N.YILDIZ';
 
     public static function send($number, $message)
     {
